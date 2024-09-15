d2f963588d7f30209b0c02d05b63eb47b1b56532
Martin Flischman <martincodes@icloud.com>
2024-09-15 18:21:22 +0200

    Initial Commit

---
+++ Course List/Course_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_ListApp.swift
+//  Course List
+//
+//  Created by Martin on 2024/09/15.
+//
+
+import SwiftUI
+
+@main
+struct Course_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
